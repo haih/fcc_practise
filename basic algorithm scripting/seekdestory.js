fe6e0d6b81b46578f31e3713b68a5853a85af5b2
Hai Hu <huhai2005@126.com>
2016-02-24 22:21:37 +0800

    3 params
    add some comments

--- basic algorithm scripting/seekdestory.js
+++ basic algorithm scripting/seekdestory.js
@@ -1,14 +1,17 @@
-function destroyer(arr) {
+/*
+You will be provided with an initial array (the first argument in the destroyer function),
+ followed by one or more arguments. Remove all elements from the initial array that are of 
+ the same value as these arguments.
+*/
+function destroyer(arr,param1,param2,param3) {
   // Remove all the values
-  console.log(arr);
-  
-  var iniarray = arr[0];
+  var iniarray = arr;
   var ret = iniarray.filter(function(item){
-    if(item !== arr[1] && item !== arr[2]){
+    if(item !== param1 && item !== param2 && item !== param3){
       return item;
     }
   });
   return ret;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
