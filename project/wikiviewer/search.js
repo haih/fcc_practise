4d524b3dafc913376b370d161ff79bf633ff4db7
haih <huhai2005@126.com>
2016-08-10 21:53:18 +0800

    add the wiki viewer completed.

--- project/wikiviewer/search.js
+++ project/wikiviewer/search.js
@@ -1,16 +1,26 @@
-var app = angular.module('wikiViewer', []);
-
-
-app.factory('SearchApi',function($http){
-    var obj = {};
-    var searchURL = 'http://en.wikipedia.org/w/api.php?action=query&generator=search&prop=extracts&exsentences=1&exlimit=max&exintro&explaintext=1&format=json&callback=JSON_CALLBACK&gsrsearch=';
-    obj.getResult = function() {
-    return $http.jsonp(searchURL);
-  };
-    
-})
-
-app.controller('MainCtrl', function() {
-    var vm = this;
-    vm.title = "anything";
+$(function() {
+    $("#search").keyup(function(e){
+        var q = $("#search").val();
+        $.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
+        {
+          srsearch: q,
+          action: "query",
+          list: "search",
+          format: "json"
+        },
+        function(data) {
+          $("#results").empty();
+          $("#results").append("<p>Results for <b>" + q + "</b></p>");
+          $.each(data.query.search, function(i,item){
+            $("#results").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a><br>" + item.snippet + "<br><br></div>");
+          });
+        });
+    });
 });
+
+function RandomWindow(){
+    var randomUrl = "https://en.wikipedia.org/wiki/Special:Random";
+    window.open(randomUrl);
+    return ;
+}
+
---
+++ reactjs/demo1/js/index.js
@@ -0,0 +1,18 @@
+
+'use strict';
+var React = require('react');
+
+
+var Title = React.createClass({
+
+    
+})
+
+
+
+ReactDOM.render(
+    <h1>Hello world</h1>,
+    document.getElementById('demo1')
+);
+
+module.export = Title;
